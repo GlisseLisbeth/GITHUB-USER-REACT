1fb483de36bbb38ae365f60d169c82d089148727
glisselisbethjm <glisse1986.jm@outlook.com>
2019-05-27 11:48:07 -0500

    Updates

---
+++ precache-manifest.0c7170c59ccd00c21839830db424e1fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b61f2f718763aeecfa1618014f1b92b1",
+    "url": "/GITHUB-USER-REACT/index.html"
+  },
+  {
+    "revision": "ecfd8b09c972e501edb1",
+    "url": "/GITHUB-USER-REACT/static/css/main.f6151144.chunk.css"
+  },
+  {
+    "revision": "ce57ad34be37f6a03cc8",
+    "url": "/GITHUB-USER-REACT/static/js/2.7eea2e3d.chunk.js"
+  },
+  {
+    "revision": "ecfd8b09c972e501edb1",
+    "url": "/GITHUB-USER-REACT/static/js/main.51c35950.chunk.js"
+  },
+  {
+    "revision": "e404dd5b22fe26477daf",
+    "url": "/GITHUB-USER-REACT/static/js/runtime~main.2e25ba96.js"
+  },
+  {
+    "revision": "b874ac629cfe8dc6e6836edf43416c30",
+    "url": "/GITHUB-USER-REACT/static/media/github.b874ac62.png"
+  }
+]);
